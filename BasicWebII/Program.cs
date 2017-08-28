8e18858a35dcbfd3a45422eea0bad85c2ad3571e
clbokea <clbo@kea.dk>
2017-08-28 13:31:11 +0200

    basicw

---
+++ BasicWebII/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace BasicWebII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = WebHost.CreateDefaultBuilder().UseStartup<Startup>().Build();
+            host.Run();
+
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
